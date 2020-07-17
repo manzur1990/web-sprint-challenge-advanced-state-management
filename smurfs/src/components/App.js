e3c3a93e02f01ed1e6ae566568e41cd14d8aeb6b
JM <manzur-1990@hotmail.com>
2020-07-17 12:33:27 -0500

    MVP + Stretch, working on setting Usereducer to the project

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,17 +2,61 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 import { SmurfContext } from './context/SmurfContext'
-import { SmurfCard } from './SmurfCard'
-import { SmurfForm } from './SmurfForm'
-
+import  SmurfList from './SmurfList'
+import  SmurfForm  from './SmurfForm'
 
 
 export const App = () => {
+  const [smurfs, setSmurfs] = useState([])
+const [newSmurf, setNewSmurf] = useState({
+  name:'',
+  age:'',
+  height:'',
+  id:''
+})
+  
+useEffect(() =>{
+  axios.get('http://localhost:3333/smurfs')
+  .then(res=>{
+    setSmurfs(res.data)
+  })
+},[])
+
+const onChange = e => {
+  const smurf = e.target.name
+  setNewSmurf({
+      ...newSmurf,
+      [smurf]: e.target.value, id: Date.now()
+  })
+}
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  axios.post("http://localhost:3333/smurfs", {...newSmurf})
+    .then((res) => setSmurfs(res.data))  
+    .catch((err) => {
+      console.log("post: ", err.response);
+    });
+    setNewSmurf("")
+};
+
+const removeSmurf = smurfId => {
+ axios
+ .delete(`http://localhost:3333/smurfs/${smurfId}`)
+ .then(res =>{
+  setSmurfs(res.data)
+ })
+ .catch(err =>{
+  console.log(err.data)
+ })
+ 
+}
+  
   return (
     
-      <SmurfContext.Provider value={{}}>
-      <SmurfCard/>
-      <SmurfForm/>
+      <SmurfContext.Provider value={{onSubmit, smurfs, onChange, removeSmurf}}>
+      <SmurfForm /> 
+      <SmurfList />
     </SmurfContext.Provider>
     
   )
--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { SmurfContext } from './context/SmurfContext'
 
-export const SmurfForm = () => {
+
+
+export default function SmurfForm() {
+    const {onSubmit, onChange} = useContext(SmurfContext)
+
     return (
         <div>
-            
+            <h3>Describe your Smurf</h3>
+            <form onSubmit={onSubmit}>
+                <input 
+                type='text'
+                name='name'
+                placeholder='name'
+                onChange={onChange}
+                /> 
+                <br/>
+                <input 
+                type='text'
+                name='age'
+                placeholder='age'
+                onChange={onChange}
+                /> 
+                <br/>
+                <input 
+                type='text'
+                name='height'
+                placeholder='height'
+                onChange={onChange}
+                /> 
+                <br/>
+
+                <button className="button">+</button>
+                
+            </form>
         </div>
     )
 }
